60eadc8665e3da9fd8d03b62b90caa69c966b076
Charles Azevedo <vx.charles@gmail.com>
2021-05-09 20:39:45 -0300

    Imported useContext, contextHero, excludeEquipe, useDispatch and useSelector. Adjusted the code to add new functions to view and remove character details directly on the selected team and removed bindActionCreator and connect.

--- src/components/equipes.jsx
+++ src/components/equipes.jsx
@@ -1,52 +1,80 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { selectedEquipe } from '../store/actions/characterActions'
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { excludeEquipe, selectHero } from '../store/actions/characterActions'
 
 import '../css/equipes.css'
-import close from '../images/cancel.svg'
+import closeSVG from '../images/cancel.svg'
+
+import { useContext } from 'react'
+import ContextHero from '../context/contextHero'
 
 const Equipes = props => {
 
-  function closeEquipe() {
-    // document.getElementsByClassName("selected-heroes")[0].classList.add('active')
-    document.querySelector(".selected-heroes").classList.remove('active')
-    document.querySelector(".ver-lista").classList.remove("d-none")
-    
-  }  
+  const listEquip = useSelector(state => ({ equipe: state.characters.equipe, limitEquip: state.characters.limitEquip }))
+  const dispatch = useDispatch({ excludeEquipe, selectHero })
+
+  const { setCloseDrawer } = useContext(ContextHero)
+
+  const [verLista, setVerLista] = useState()
+  const [close, setClose] = useState()
+  
+  function showEquipes() {
+    if(listEquip.limitEquip > 0){
+      setClose(true)
+    }
+  }
+
+  function closeEquipe() {    
+    setClose(true)
+    if(close) {
+      setClose(false)
+    }
+  }
+
+  function openDrawerDetailEquip(equipe, id, name) {
+    setCloseDrawer(true)
+    dispatch(selectHero(equipe, id, name))
+  }
+
+  useEffect(() => {
+    if(listEquip.limitEquip > 0) {
+      setVerLista("Ver equipes")
+      setClose(true)
+    } else {
+      setVerLista("Equipe vazia")      
+      setClose(false)
+    }
+  },[listEquip.limitEquip])
 
   return (
-    <div className="selected-heroes">
-
-      {/* <div className="items">
-        <figure className="circle-img">
-        
-        </figure>
-        <span>teste</span>
-      </div> */}
-
- 
-      <div className="close-equipe">
-        <img src={close} alt="Fechar" width="30" onClick={closeEquipe} />
+    <>
+      <div className={close ? "close-equipe" : "d-none"} onClick={closeEquipe}>
+        <img src={closeSVG} alt="Fechar" width="30" />
+        Fechar
       </div>
+      <div className={close ? "d-none" : "ver-lista"} onClick={showEquipes}>
+        <span>{verLista}</span>
+      </div>
+      
+      <div className={close ? "selected-heroes active" : "selected-heroes"}>
 
+        {
+          listEquip.equipe !== 'undefined' ? listEquip.equipe.map(equipe => (
+            <div className="items" key={equipe.name}>
+              <div className="close-equipe-list">
+                <img src={closeSVG} alt="Fechar" width="30" onClick={() => dispatch(excludeEquipe(listEquip.equipe, equipe.id ))} />
+              </div>
+              <figure className="circle-img" onClick={() => openDrawerDetailEquip(listEquip.equipe, equipe.id, equipe.name)}>
+                <img src={equipe.thumbnail.path+'.jpg'} alt=""/>
+              </figure>
+              <span>{equipe.name}</span>
+            </div>
+          )) : ''
+        }
 
-      {
-        props.equipe !== 'undefined' ? props.equipe.map(equipe => (
-          <div className="items" key={equipe.name}>
-            <figure className="circle-img">
-              <img src={equipe.img} alt=""/>
-            </figure>
-            <span>{equipe.name}</span>
-          </div>
-        ))
-        : ""
-      }
-
-    </div>
+      </div>
+    </>
   )
 }
 
-const mapStateToProps = state => ({ equipe: state.characters.equipe })
-const mapDispatchToProps = dispatch => bindActionCreators({ selectedEquipe }, dispatch)
-export default connect(mapStateToProps, mapDispatchToProps)(Equipes)
+export default Equipes
