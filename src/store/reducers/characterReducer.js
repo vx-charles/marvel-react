a31d4975d699e7687ec7f772f994ca2d79526966
Charles Azevedo <vx.charles@gmail.com>
2021-05-09 20:25:33 -0300

    Adjusted the SEARCH_HERO, MINHA_EQUIPE AND EXCLUDE_EQUIPE and removed the CLICKED

--- src/store/reducers/characterReducer.js
+++ src/store/reducers/characterReducer.js
@@ -1,19 +1,17 @@
-const INITIAL_STATE = { load: true, searchHero: [], list: [], selectHeroDetail: [], total: 0, activeSearch: false, equipe: [] }
+const INITIAL_STATE = { load: true, searchHero: [], list: [], selectHeroDetail: [], total: 0, equipe: [], limitEquip: 0 }
 
 const charReducer = (state = INITIAL_STATE, action) => {
 	switch (action.type) {
-		case 'CLICKED':
-			return { ...state, click: true }		
 		case 'SELECT_HERO':
-			return { ...state, selectHeroDetail: action.payload }		
+			return { ...state, selectHeroDetail: action.payload }
 		case 'SEARCHED':
 			return { ...state, load: false, list: action.payload, total: action.payloadTotal }	
 		case 'SEARCH_HERO':
-			return { ...state, activeSearch: true, searchHero: action.payload }
+			return { ...state, searchHero: action.payload }
 		case 'MINHA_EQUIPE':
-			return { ...state, equipe: action.payload }
+			return { ...state, equipe: action.payload, limitEquip: action.payloadLimit }
 		case 'EXCLUDE_EQUIPE':
-			return { ...state, equipe: action.payload }
+			return { ...state, equipe: action.payload, limitEquip: action.payloadLimit }
 		default:
 			return state
 	}
