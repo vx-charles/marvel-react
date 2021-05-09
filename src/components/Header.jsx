be7d0c30b5688f6b6703716336daccd624cdd1cd
Charles Azevedo <vx.charles@gmail.com>
2021-05-09 20:41:41 -0300

    Adjusted the code, moving it to the equipes.jsx file.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,22 @@
 import React from 'react'
+
 import logo from '../images/logo.png'
 
 import '../css/header.css'
 import Equipes from './equipes'
 
-export function Header() {
-  
-  function showEquipes() {
-    document.querySelector(".selected-heroes").classList.add('active')
-    document.querySelector(".ver-lista").classList.add("d-none")
-  }
-
+const Header = props => {
   return (
     <header>
-      <div className="row">
-        <div className="col-12 col-md-6 logo">
-          <img src={logo} alt="Logo Marvel" />
-        </div>
-        <div className="col-12 col-md-6 equipe">
-          <div className="vazio">Equipe vazia</div>
-          <div className="ver-lista d-none" onClick={showEquipes}>Ver Equipes</div>
-          <Equipes />
-        </div>
+      <div className="logo">
+        <img src={logo} alt="Logo Marvel" />
+      </div>
+      
+      <div className="equipe">        
+        <Equipes />
       </div>
     </header>
   )
 }
+
+export default Header
