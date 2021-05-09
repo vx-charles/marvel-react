65c92f390dc1e53df51d1ea66102ed6aeca73cdb
Charles Azevedo <vx.charles@gmail.com>
2021-05-09 20:32:36 -0300

    Removed pure JS code, adjusted selectHero functions to open the drawer, selectedEquipe to add attribute add: true and excludeEquipe to exclude only 1 character.

--- src/store/actions/characterActions.js
+++ src/store/actions/characterActions.js
@@ -1,6 +1,7 @@
 import { axiosBaseURL, params } from "../../api/apiRequestHeroes"
 import _ from 'lodash'
 
+// Lista os personagens assim que iniciar a página
 export const listHeroes = () => {
   return async dispatch => {
     const request = axiosBaseURL.get('characters',
@@ -11,90 +12,87 @@ export const listHeroes = () => {
   }
 }
 
-export const selectHero = name => {
-  document.getElementsByClassName("list-heroes")[0].classList.add('activeListHeroWidth')
-  document.getElementsByClassName("detail-hero")[0].classList.add('activeDetail')
-    
-  var y = document.getElementsByClassName("col-lg-3");
-  var i;
-  for (i = 0; i < y.length; i++) {
-    y[i].classList.add('col-lg-6');
-  }
+// Seleciona o personagem assim que clica no botão ver mais.
+export const selectHero = (arrHero, id, name) => {
+   
+  // retorna somente o herói selecionado que existe no arrHero, quando ocorre o scroll infinito, havendo a adição de objetos no array.
+  const heroFilter = _.filter(arrHero, function(obj) { return obj.id === id })
 
   return async dispatch => {
-    const request = axiosBaseURL.get('characters', {
-      params: {
-        ts: 6,
-        apikey: '60a102569ba4ccc05c57af19ebeaf636',
-        hash: 'c96130248d43dbb1b0583dde5de35803',
-        name: name
-      }
-    })
-    const resp = await request
-    return dispatch({ type: 'SELECT_HERO', payload: resp.data.data.results })
-  }
-}
-
-export const search = name => {
-  
-    return async dispatch => {
-      if(name !== "") {
-        document.getElementsByClassName("cards")[0].classList.add('d-none')
-        
-        const request = axiosBaseURL.get('characters', {
-          params: {
-            ts: 6,
-            apikey: '60a102569ba4ccc05c57af19ebeaf636',
-            hash: 'c96130248d43dbb1b0583dde5de35803',
-            name: name
-          }
-        })
-        const resp = await request
-        return [
-          dispatch({ type: 'SEARCH_HERO', payload: resp.data.data.results }),
-          document.getElementsByClassName("no-hero")[0].classList.remove('d-none')
-        ]
-      } else {
-        return [
-          dispatch({ type: 'SEARCH_HERO', payload: [] }),
-          document.getElementsByClassName("cards")[0].classList.remove('d-none'),
-          document.getElementsByClassName("no-hero")[0].classList.add('d-none'),                    
-          listHeroes()
-        ]
-      }
-      
+    if(heroFilter !== ''){
+      return dispatch({ type: 'SELECT_HERO', payload: heroFilter })
+    } else {
+      const request = axiosBaseURL.get('characters', {
+        params: {
+          ts: 6,
+          apikey: '60a102569ba4ccc05c57af19ebeaf636',
+          hash: 'c96130248d43dbb1b0583dde5de35803',
+          name: name
+        }
+      })
+      const resp = await request
+      return dispatch({ type: 'SELECT_HERO', payload: resp.data.data.results })
     }
-  
+  } 
 }
 
-export const selectedEquipe = equipe => {
-  document.getElementsByClassName("vazio")[0].classList.add("d-none")
-  document.getElementsByClassName("selected-heroes")[0].classList.add("active")
-  
-  let listEquip = equipe.filter(function (a) {
-    return !this[JSON.stringify(a)] && (this[JSON.stringify(a)] = true);
-  }, Object.create(null))
-  
+// pesquisa o personagem no input
+export const search = name => {
+  return async dispatch => {
+    if(name !== "") {
+      
+      const request = axiosBaseURL.get('characters', {
+        params: {
+          ts: 6,
+          apikey: '60a102569ba4ccc05c57af19ebeaf636',
+          hash: 'c96130248d43dbb1b0583dde5de35803',
+          name: name
+        }
+      })
 
-  if(listEquip.length >= 5) {
-    document.querySelector(".adicionar").classList.add('d-none')
-    document.querySelector(".excluir").classList.remove('d-none')
-  } else {
-    document.querySelector(".adicionar").classList.remove('d-none')
-    document.querySelector(".excluir").classList.add('d-none')
-  }
+      const resp = await request
+      
+      if(resp.data.data.count > 0) { // quando na requisição o atributo tem count maior que 1, possui resultado.
+        resp.data.data.results[0].search = true // add um objeto no array de objetos na pesquisa.
+        dispatch({ type: 'SEARCH_HERO', payload: resp.data.data.results })
+      } else { // retorna um objeto com informações de pesquisa não encontrada.
+        dispatch({ type: 'SEARCH_HERO', payload: [{id: "x-200", name: "Herói não existe! Tente Novamente", search: true }] })
+      }
 
-  return dispatch => {
-    return dispatch({ type: 'MINHA_EQUIPE', payload: listEquip })
+    } else { // lista novamente os personagens quando a pesquisa for vazio, mandando um array vazio no payload.
+      return [
+        dispatch({ type: 'SEARCH_HERO', payload: [] }),               
+        listHeroes()
+      ]
+    }    
   }  
 }
 
-export const excludeEquipe = equipe => {
-  console.log(equipe)
-  const teste = equipe.splice(_.findIndex(equipe, {id: equipe.id}))
-  console.log(teste)
+// add o personagem na equipe assim que clicar no botão "Adicionar"
+export const selectedEquipe = (equipe) => {
+    
+  let equipFilter = equipe.filter(function (a) { // Evita de add personagens iguais.
+    return !this[JSON.stringify(a)] && (this[JSON.stringify(a)] = true);
+  }, Object.create(null))  
+  
+  equipe[equipe.length-1].add = true // add o objeto que o personagem foi adicionado no estado, no mesmo objeto do personagem selecionado nos detalhes.
 
   return dispatch => {
-    return dispatch({ type: 'EXCLUDE_EQUIPE', payload: teste })
+    return dispatch({ type: 'MINHA_EQUIPE', payload: equipFilter, payloadLimit: equipe.length })
+  }  
+}
+
+// Exclui o personagem na equipe assim que clicar no botão "Excluir"
+export const excludeEquipe = (equipe, id) => {
+  
+  const newEquipe = _.remove(equipe, function(obj){ 
+    if(obj.id === id){
+      obj.add = false // add o objeto {add: false} no mesmo array onde exibe os detalhes do personagem apenas, sem remover do array.
+    }
+    return !_.isEqual(id, obj.id); //utiliza o isEqual para comparar
+  })
+  
+  return dispatch => {
+    return dispatch({ type: 'EXCLUDE_EQUIPE', payload: newEquipe, payloadLimit: newEquipe.length })
   } 
 }
