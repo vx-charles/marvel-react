f3ada36a635fa873693452d893606af11375175a
vx-charles <tecnologialev@golev.com.br>
2021-05-13 17:44:15 -0300

    Altered public key and hash

--- src/api/apiRequestHeroes.js
+++ src/api/apiRequestHeroes.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 // import md5 from 'md5'
 
-const publicKey = "60a102569ba4ccc05c57af19ebeaf636"
+const publicKey = "b851322ff45094f53be8e488f1c3db76"
 const ts = 6
-const hash = "c96130248d43dbb1b0583dde5de35803" // md5(ts + privateKey + publicKey) nesta ordem, inverter a ordem a hash não é a mesma.
+const hash = "3899499c303701d1eed4542ec134f4da" // md5(ts + privateKey + publicKey) nesta ordem, inverter a ordem a hash não é a mesma.
 
 export const params = {
   ts: `${ts}`,
