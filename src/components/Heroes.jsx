53e20f1aa18ea6b93555e8da56cfe1eb8dbaeb8a
vx-charles <tecnologialev@golev.com.br>
2021-05-13 17:47:47 -0300

    Added components to the code and optimized the code

--- src/components/Heroes.jsx
+++ src/components/Heroes.jsx
@@ -1,40 +1,37 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState, useContext } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { listHeroes, selectHero, search } from '../store/actions/characterActions'
+import { listHeroes, search } from '../store/actions/characterActions'
 import InfiniteScroll from 'react-infinite-scroller'
 import axios from 'axios'
 
 import '../css/listHeroes.css'
-import eye from '../images/visibility.svg'
 import loading from '../images/loading.svg'
-import searchIcon from '../images/search.svg'
-import interrogationIcon from '../images/interrogation.svg'
 
-import { useContext } from 'react'
 import ContextHero from '../context/contextHero'
 
+// COMPONENTES
+import { CardSearch } from './CardSearch'
+import { SearchFrom } from './SearchForm'
+import { ListInfiniteHeroes } from './ListInfiniteHeroes'
 
-function Heroes(props) {
+function Heroes() {
 
   const stateProps = useSelector(state => (
     { 
       list: state.characters.list, 
       load: state.characters.load, 
-      selectHeroDetail: state.characters.selectHeroDetail, 
       total: state.characters.total, 
       searchHero: state.characters.searchHero 
     } 
   ))
 
-  const dispatch = useDispatch({ listHeroes, selectHero, search })
+  const dispatch = useDispatch({ listHeroes, search })
 
-  const { closeDrawer, setCloseDrawer } = useContext(ContextHero)
+  const { closeDrawer } = useContext(ContextHero)
 
   const initialRender = useRef(true) // obtém a referência na DOM para armazenar dados.
   const [list, setList] = useState(stateProps.load === false ? stateProps.list.results : [])
-  const [valor, setValor] = useState('') // usado para pegar o valor na pesquisa.
   const [offset, setOffset] = useState(0) // faz o offset pegando de 20 em 20 a cada requisição.
-  const [isVisibleClear, setIsVisibleClear] = useState(false)
   const limit = 20
 
   useEffect(() => {
@@ -50,8 +47,8 @@ function Heroes(props) {
       const result = await axios.get('https://gateway.marvel.com/v1/public/characters', {
         params: {
           ts: 6,
-          apikey: "60a102569ba4ccc05c57af19ebeaf636",
-          hash: "c96130248d43dbb1b0583dde5de35803",
+          apikey: "b851322ff45094f53be8e488f1c3db76",
+          hash: "3899499c303701d1eed4542ec134f4da",
           limit: limit,
           offset: offset
         }
@@ -65,72 +62,19 @@ function Heroes(props) {
         setOffset(offset + 20)
       }      
     }, 1000);
-  }  
-
-  function keyHandler(e) {
-    if (e.key === 'Enter') {
-      dispatch(search(valor))
-      setIsVisibleClear(true)
-    }
-  }
-
-  function clearInput() {
-    setValor("")
-    dispatch(search(""))
-    setIsVisibleClear(false)
-  }
-  
-  function searchInput(valor) {
-    setIsVisibleClear(true)
-    dispatch(search(valor))
-  }
-
-  function detailChar(arr, id, name) {
-    setCloseDrawer(true)
-    dispatch(selectHero(arr, id, name))
   }
 
   return (
     <div className={closeDrawer ? "list-heroes activeListHeroWidth" : "list-heroes"}>
       <div className="container">
 
-        <div className="form-container">
-          <input className="form-search" type="text" placeholder="Busca" value={valor} onKeyUp={keyHandler} onChange={(e) => setValor(e.target.value)} />
-          <button type="button" className="search-btn" onClick={() => searchInput(valor) }>
-            <img src={searchIcon} alt="Search icon" />
-          </button>
-          <button type="button" className={isVisibleClear ? "limpar" : "limpar d-none"} onClick={() => clearInput()}>Limpar pesquisa</button>
-        </div>        
+        <SearchFrom />
         
-        <div className="card-search">            
-          { // gera a lista somente do herói pesquisado
-            stateProps.searchHero[0]?.id !== "x-200" ? stateProps.searchHero.map(char => (
-              <div className="search-list card" key={char.id}>
-                <div className="itens">
-                  <img src={char.thumbnail?.path + '/standard_xlarge.' + char.thumbnail?.extension} alt={char.name} />
-                  <div className="info">
-                    <span>{char.name}</span>
-                    <button className="ver-mais" onClick={() => detailChar(stateProps.searchHero, char.id, char.name)}>
-                      <img src={eye} alt="eye" />
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))
-            :
-            stateProps.searchHero.map(char => (
-              <div className="not-found card" key={char.id}>
-                <div className="itens">
-                <img src={interrogationIcon} alt="interrogação icon" />
-                  <div className="info">
-                    <span>{char.name}</span>
-                  </div>
-                </div>
-              </div>
-            ))
-          }          
+        <div className="card-search">
+          <CardSearch />
         </div>
 
+        {/* INIFINTE SCROLL */}
         <InfiniteScroll
           pageStart={0}
           loadMore={getMoreHeroes}
@@ -139,21 +83,10 @@ function Heroes(props) {
           className={stateProps.searchHero[0]?.search ? "d-none" : "cards"}
         >
           {
-            stateProps.load === false ?
-                list.map(char => (
-                  <div className="card" key={char.id}>
-                    <div className="itens">
-                      <img src={char.thumbnail.path + '/standard_xlarge.' + char.thumbnail.extension} alt={char.name} />
-                      <div className="info">
-                        <span>{char.name}</span>
-                        <button className="ver-mais" onClick={() => detailChar(list, char.id, char.name)}>
-                          <img src={eye} alt="eye" />
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                ))
-            : ''              
+            stateProps.load === false ? list.map(char => (
+              <ListInfiniteHeroes arrObjChar={char} listStateProps={list} key={char.id} />
+            ))
+            : <img className="loading-svg" src={loading} alt="Loading..." />
           }
         </InfiniteScroll>      
 
